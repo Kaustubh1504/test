1b0c00c2aaf09284f56e8811f3f8633f82cc78cb
Kaustubh Gharat <122547394+Kaustubh1504@users.noreply.github.com>
2023-11-19 22:00:57 +0530

    Add files via upload

---
+++ src/context/authContext.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext();
+const baseurl = "http://localhost:8000"
+export const AuthContexProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(localStorage.getItem("user")) || null
+  );
+
+  const login = async (inputs) => {
+    
+    console.log(inputs)
+    try{
+      const res = await axios.post(`${baseurl}/login`,
+      {headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Credentials": true,
+      }},
+      inputs);
+      setCurrentUser(res.data);
+    }catch(err){
+      console.error('Login error:', err);
+    }
+    
+  };
+
+  const logout = async (inputs) => {
+    await axios.post(`${baseurl}/logout`);
+    setCurrentUser(null);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(currentUser));
+  }, [currentUser]);
+
+  return (
+    <AuthContext.Provider value={{ currentUser, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
---
+++ src/pages/Single.jsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from "react";
+import Edit from "../img/edit.png";
+import Delete from "../img/delete.png";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import Menu from "../components/Menu";
+import axios from "axios";
+import moment from "moment";
+import { useContext } from "react";
+import { AuthContext } from "../context/authContext";
+import DOMPurify from "dompurify";
+
+const Single = () => {
+  const baseurl = "http://localhost:8000"
+  const [post, setPost] = useState({});
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const postId = location.pathname.split("/")[2];
+
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`${baseurl}/read/${postId}`);
+        setPost(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [postId]);
+
+  const handleDelete = async ()=>{
+    try {
+      await axios.delete(`${baseurl}/delete/${postId}`);
+      navigate("/")
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const getText = (html) =>{
+    const doc = new DOMParser().parseFromString(html, "text/html")
+    return doc.body.textContent
+  }
+
+  return (
+    <div className="single">
+      <div className="content">
+        {/* <img src={`../upload/${post?.img}`} alt="" /> */}
+        <img src={post.img} alt="" />
+        <div className="user">
+          {/* {post.userImg && <img
+            src={post.userImg}
+            alt=""
+          />} */}
+          <div className="info">
+            <p>Posted {moment(post.date).fromNow()}</p>
+          </div>
+          {(
+            <div className="edit">
+              <Link to={`/write`} state={post}>
+                <img src={Edit} alt="" />
+              </Link>
+              <img onClick={handleDelete} src={Delete} alt="" />
+            </div>
+          )}
+        </div>
+        <h1>{post.title}</h1>
+        <p
+          dangerouslySetInnerHTML={{
+            __html: DOMPurify.sanitize(post.desc),
+          }}
+        ></p>
+        </div>
+      <Menu cat={post.cat}/>
+    </div>
+  );
+};
+
+export default Single;
