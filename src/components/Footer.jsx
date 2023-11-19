e42aab6a91a04c3e6f944d1f8b22bbe4cdc6b4da
Kaustubh Gharat <122547394+Kaustubh1504@users.noreply.github.com>
2023-11-19 22:43:25 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import Logo from "../img/logo.png";
 const Footer = () => {
   return (
     <footer>
-      <img src={Logo} alt="" />
+      <img src="https://1000logos.net/wp-content/uploads/2020/08/Blogger-Logo-2010.png" alt="" />
       <span>
         Made with ♥️ and <b>React.js</b>.
       </span>
